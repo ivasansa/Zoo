652826e84e553a69649017602d006dc947fcf33d
Marc Llobera <mallro1516daw2@gmail.com>
2015-11-13 18:26:07 +0100

    “Versió 1.1 del programa bomba”

--- src/zoo/Zona.java
+++ src/zoo/Zona.java
@@ -15,38 +15,67 @@ public class Zona {
 
     
     
+    /**
+     * Getter del atribut ID
+     * @return DNI que retorna el ID de la Classe Zona
+     */
     public int getID() {
         return ID;
     }
-
+/**
+     * Setter del atribut ID
+     * @param ID que es un integer de la Classe Zona 
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
-
+/**
+     * Getter del atribut nom
+     * @return nom que retorna el nom de la Classe Zona
+     */
     public String getNom() {
         return nom;
     }
-
+/**
+     * Setter del atribut nom
+     * @param nom que es un integer de la Classe Zona 
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
-
+/**
+     * Getter del atribut superficie
+     * @return superficie que retorna la superficie de la Classe Zona
+     */
     public int getSuperficie() {
         return superficie;
     }
-
+/**
+     * Setter del atribut superficie
+     * @param superficie que es un integer de la Classe Zona 
+     */
     public void setSuperficie(int superficie) {
         this.superficie = superficie;
     }
-
+/**
+     * Getter del atribut net
+     * @return net que retorna l'estat higienic de la Classe Zona
+     */
     public boolean isNet() {
         return net;
     }
-
+/**
+     * Setter del atribut net
+     * @param net que es un boolean de la Classe Zona 
+     */
     public void setNet(boolean net) {
         this.net = net;
     }
-
+    
+    /**
+     * Reimplementacio  del metode toString de l'objecte Object
+     * @return  retorna la localitzacio de l'animal
+     */
     @Override
     public String toString() {
         return "L'animal està a la zona "+nom;
